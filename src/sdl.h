5f89fcb0b6721cea6600eb80727c046bb55870de
Dani Rodríguez <danirod@outlook.com>
2015-03-10 14:31:44 +0100

    Didn't notice didn't include headers
    This is not strictly related to issue 3 but I have noticed this
    when started working on this, so I'm fixing it right now. It seems
    the code still compiles as some other header files already include
    SDL2, however, it's bad anyway.

--- src/sdl.h
+++ src/sdl.h
@@ -1,5 +1,15 @@
-#ifndef MY_SDL_H_
-#define MY_SDL_H_
+/*
+ * After checking out SDL2 source code it has been found that the include
+ * guard for SDL2/SDL.h is named _SDL_H. Since coding conventions for this
+ * project have stablished that the underscore should go AFTER the H, a
+ * conflict may not happen. Let's just hope that SDL team never changes
+ * idea. (Why would they?)
+ */
+#ifndef SDL_H_
+#define SDL_H_
+
+#include "cpu.h"
+#include <SDL2/SDL.h>
 
 struct context_t
 {
@@ -16,4 +26,4 @@ void destroy_context(struct context_t*);
 
 void render(struct context_t*, struct machine_t*);
 
-#endif // MY_SDL_H_
+#endif // SDL_H_
