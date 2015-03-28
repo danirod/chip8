ee5c54f3f812157e52796474fb2c99273441917e
Angel <mandevone@gmail.com>
2015-03-28 19:06:05 +0000

    formating nada relevante

--- src/cpu.h
+++ src/cpu.h
@@ -29,15 +29,15 @@
  */
 struct machine_t
 {
-    uint8_t mem[MEMSIZ];        // Memory is allocated as a buffer
+    uint8_t  mem[MEMSIZ];       // Memory is allocated as a buffer
     uint16_t pc;                // Program Counter
 
     uint16_t stack[16];         // Stack can hold 16 16-bit values
     uint16_t sp;                // Stack pointer
 
-    uint8_t v[16];              // 16 general purpose registers
+    uint8_t  v[16];             // 16 general purpose registers
     uint16_t i;                 // Special I register
-    uint8_t dt, st;             // Timers
+    uint8_t  dt, st;            // Timers
 
     char screen[2048];          // Screen bitmap
     char wait_key;              // Key the CHIP-8 is idle waiting for.
@@ -49,36 +49,34 @@ typedef void (*procesarInstruccionesOpcodes)(struct machine_t*, uint16_t, uint16
 procesarInstruccionesOpcodes instruccion_p;
 procesarInstruccionesOpcodes instruccion_n;
 procesarInstruccionesOpcodes instruccion_kk;
-
  
 void init_machine(struct machine_t*);
 
 void step_machine(struct machine_t*);
 
-
-/*===================================================================================================                        
- *=============    Nomenclatura para comprender el nombre de las fuciones añadidas:    ==============                        
- *===================================================================================================                        
- *===    void spC1_1NNN (...);                                                                    ===                        
- *===                                                                                             ===                        
- *===    sp = "s" se refiere a switch, "p" a la expresion que se usaba           ej: switch (p) { ===                        
- *===    C1 = "C" se refiere a case,   "1" a la expresion constante que se usaba ej: case 1 :     ===                        
- *===                                                                                             ===                        
- *===    _1NNN = "_" se usa para que sea mas legible,                                             ===                        
- *===            "1NNN" se refiere a las instrucciones que maneja dentro,                         ===                        
- *===            si ahy varias, se reflejaran las que consten separandolas,                       ===                        
- *===            por "_", a no ser que anteriormente se manejaran mediante,                       ===                        
- *===            un switch, en ese caso se pondria la nomenclatura                                ===                        
- *===            para el switch cita anteriormente.                                               ===                        
- *===                                                                                             ===                        
- *===    ej: para dos instrucciones:                                                              ===                        
- *===        spC0_00E0_00EE                                                                       ===                        
- *===                                                                                             ===                        
- *===    ej: para una situacion en la que anteriormente se usaba in switch anidado:               ===                        
- *===        spC8_sn.                                                                             ===                        
- *===                                                                                             ===                        
- *===                                                                                             ===                        
- *===        Nota: Ahora se manejara dentro otro array de punteros a funciones.                   ===                        
+/*===================================================================================================                       
+ *=============    Nomenclatura para comprender el nombre de las fuciones añadidas:    ==============                       
+ *===================================================================================================                       
+ *===    void spC1_1NNN (...);                                                                    ===                       
+ *===                                                                                             ===                       
+ *===    sp = "s" se refiere a switch, "p" a la expresion que se usaba           ej: switch (p) { ===                       
+ *===    C1 = "C" se refiere a case,   "1" a la expresion constante que se usaba ej: case 1 :     ===                       
+ *===                                                                                             ===                       
+ *===    _1NNN = "_" se usa para que sea mas legible,                                             ===                       
+ *===            "1NNN" se refiere a las instrucciones que maneja dentro,                         ===                       
+ *===            si ahy varias, se reflejaran las que consten separandolas,                       ===                       
+ *===            por "_", a no ser que anteriormente se manejaran mediante,                       ===                       
+ *===            un switch, en ese caso se pondria la nomenclatura                                ===                       
+ *===            para el switch cita anteriormente.                                               ===                       
+ *===                                                                                             ===                       
+ *===    ej: para dos instrucciones:                                                              ===                       
+ *===        spC0_00E0_00EE                                                                       ===                       
+ *===                                                                                             ===                       
+ *===    ej: para una situacion en la que anteriormente se usaba in switch anidado:               ===                       
+ *===        spC8_sn.                                                                             ===                       
+ *===                                                                                             ===                       
+ *===                                                                                             ===                       
+ *===        Nota: Ahora se manejara dentro otro array de punteros a funciones.                   ===                       
  *===================================================================================================*/
 
 
@@ -90,22 +88,16 @@ void spC1_1NNN        (struct machine_t*, uint16_t, uint16_t, uint8_t,
 
 void spC2_2NNN        (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //2NNN: CALL NNN                                                 
-
 void spC3_3XKK        (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //3XKK: SE   X, KK                                               
-
 void spC4_4XKK        (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //4XKK: SNE  X, KK                                               
-
 void spC5_5XY0        (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //5XY0: SE   X, Y                                                
-
 void spC6_6XKK        (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //6XKK: LD   X, KK                                               
-
 void spC7_7XKK        (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //7XKK: ADD  X, KK                                               
-
 void spC8_sn          (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t);
 
@@ -114,19 +106,14 @@ void spC9_9XY0        (struct machine_t*, uint16_t, uint16_t, uint8_t,
 
 void spC0xA_ANNN      (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t);  //ANNN: LD I, NNN                                               
-
 void spC0xB_BNNN      (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t);  //BNNN: JP V0, NNN                                              
-
 void spC0xC_CXKK      (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t);  //CXKK: RND X, KK                                               
-
 void spC0xD_DXYN      (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t);  //DXYN: X, Y, N                                                 
-
 void spC0xE_EX9E_EXA1 (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t);  //EX9E: SKP X "or" EXA1: SKNP X "in if"                         
-
 void spC0xF_skk       (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t);
 
@@ -136,53 +123,40 @@ void snC0_8XY0        (struct machine_t*, uint16_t, uint16_t, uint8_t,
 
 void snC1_8XY1        (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //8XY1: OR   X, Y                                                
-
 void snC2_8XY2        (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //8XY2: AND  X, Y                                                
-
 void snC3_8XY3        (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //8XY3: XOR  X, Y                                                
-
 void snC4_8XY4        (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //8XY4: ADD  X, Y                                                
-
 void snC5_8XY5        (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //8XY5: SUB  X, Y                                                
-
 void snC6_8X06        (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //8X06: SHR  X                                                   
-
 void snC7_8XY7        (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //8XY7: SUBN X, Y                                                
-
 void snC0xE_8X0E      (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //8X0E: SHL  X 
 
 //instrucciones que usaba kk en el antiguo swich                                                                             
 void skkC0x07_FX07    (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //FX07: LD X,   DT                                               
-
 void skkC0x0A_FX0A    (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //FX0A: LD X,   K                                                
-
 void skkC0x15_FX15    (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //FX15: LD DT,  X                                                
-
 void skkC0x18_FX18    (struct machine_t*, uint16_t, uint16_t, uint8_t,
 		       uint8_t, uint8_t, uint8_t, uint8_t); //FX18: LD ST,  X                                                
-
 void skkC0x1E_FX1E    (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //FX1E: ADD I,  X                                                
 
 void skkC0x29_FX29    (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //FX29: LD F,   X                                                
-
 void skkC0x33_FX33    (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //FX33: LD B,   X                                                
-
 void skkC0x55_FX55    (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //FX55: LD [I], X                                                
-
 void skkC0x65_FX65    (struct machine_t*, uint16_t, uint16_t, uint8_t,
                        uint8_t, uint8_t, uint8_t, uint8_t); //FX65: LD X,   [I]  
+
 #endif // CPU_H_
